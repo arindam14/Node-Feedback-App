c1c19042a3550dc3b6a5f665ca19cce2763164c0
arindamchanda <arindamchanda.86@gmail.com>
2017-10-26 02:16:07 +0530

    first commit

---
+++ config/dev.js
@@ -0,0 +1,12 @@
+// dev keys here. Don't commit this
+module.exports = {
+   googleClientID: '886308888758-6l9207h93s2f8hm7s64jbbfm6gd5arvb.apps.googleusercontent.com',
+   googleClientSecret:  'Bw4V2YTYLaIv8CBsE3Ml3nza',
+   mongoURI: 'mongodb://arindam:password@ds049848.mlab.com:49848/emaily-dev',
+   cookieKey: 'dsadsafavefwszvdsvsdascassvsdsdvds',
+   stripePublishableKey: 'pk_test_Ay2doUdAkQNXe8sAnYvRu8yu',
+   stripeSecretKey: 'sk_test_y5ZZm4jLTYfEEzvIkat6DQdZ',
+   sendGridKey:'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',
+   redirectDomain: 'http://localhost:3000'
+
+};
